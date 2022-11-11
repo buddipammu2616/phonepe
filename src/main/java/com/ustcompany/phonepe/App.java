c7b434168de3195139808a6310a912c012868834
mahesh <buddipammu2616@gmail.com>
2022-11-11 18:19:09 +0530

    unnecessary files hide

---
+++ src/main/java/com/ustcompany/phonepe/App.java
@@ -0,0 +1,13 @@
+package com.ustcompany.phonepe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
